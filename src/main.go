56ca26c1c690fff560ba74a18fa60ac1c237b0d5
NamedKitten <kitteh@namedkitten.cf>
2017-09-03 23:04:48 +0100

    Added stuff and broke submodules...

--- src/main.go
+++ src/main.go
@@ -14,10 +14,18 @@ import (
 	"strings"
 	"syscall"
 	"time"
+	"github.com/wcharczuk/go-chart"
+	"io/ioutil"
 )
 
 var redisclient *redis.Client
 var dg, _ = discordgo.New()
+var memAllocMapX []float64
+var memAllocMapY []float64
+var memSysMapX []float64
+var memSysMapY []float64
+var memTotalAllocMapX []float64
+var memTotalAllocMapY []float64
 
 func setup() {
 	reader := bufio.NewReader(os.Stdin)
@@ -41,6 +49,43 @@ func setup() {
 	redisclient.Set("prefix", prefix, 0)
 	redisclient.Set("token", strings.TrimSpace(token), 0)
 	fmt.Println("The bot is now setup.")
+}
+
+func saveMemMap() {
+	graph := chart.Chart{
+		XAxis: chart.XAxis{
+			Style: chart.Style{Show: true},
+		},
+		YAxis: chart.YAxis{
+			Style: chart.Style{Show: true},
+		},
+		Background: chart.Style{
+			Padding: chart.Box{
+				Top:  20,
+				Left: 20,
+			},
+		},
+		Series: []chart.Series{
+			chart.ContinuousSeries{
+				Name:    "MemAlloc",
+				XValues: memAllocMapX,
+				YValues: memAllocMapY,
+			},
+			chart.ContinuousSeries{
+				Name:    "MemSys",
+				XValues: memSysMapX,
+				YValues: memSysMapY,
+			},
+			chart.ContinuousSeries{
+				Name:    "MemTotalAlloc",
+				XValues: memTotalAllocMapX,
+				YValues: memTotalAllocMapY,
+			},
+		},
+	}
+	buffer := bytes.NewBuffer([]byte{})
+	_ := graph.Render(chart.PNG, buffer)
+	ioutil.WriteFile("out.png", buffer, 0644)
 }
 
 func init() {
@@ -109,6 +154,7 @@ func main() {
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
 
+	saveMemMap()
+
 	// Cleanly close down the Discord session.
-	dg.Close()
-}
+ b}
